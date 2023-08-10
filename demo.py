5535d66ab9adcb5536f9d0fc33d50d7daece4117
Shirley <shirleyzhan3@gmail.com>
2023-08-10 18:33:53 -0400

    Added an exponent function

--- demo.py
+++ demo.py
@@ -13,21 +13,25 @@ def divide(a, b):
     else:
         return "Cannot divide by zero"
 
+def square(a, b):
+    return a ** b;
+
 print("Simple Python Calculator")
 print("Operations:")
 print("1. Add")
 print("2. Subtract")
 print("3. Multiply")
 print("4. Divide")
+print("5. Exponent")
 
 while True:
-    choice = input("Enter operation (1/2/3/4) or 'exit' to quit: ")
+    choice = input("Enter operation (1/2/3/4/5) or 'exit' to quit: ")
 
     if choice.lower() == 'exit':
         print("Calculator closed.")
         break
 
-    if choice not in ['1', '2', '3', '4']:
+    if choice not in ['1', '2', '3', '4', '5']:
         print("Invalid input. Please try again.")
         continue
 
@@ -43,4 +47,6 @@ while True:
     elif choice == '4':
         result = divide(num1, num2)
 
+    elif choice == '5':
+        result = square(num1, num2)
     print("Result: ", result)
